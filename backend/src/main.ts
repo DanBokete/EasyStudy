28be0850168841a042670e27b13b47fe9d848371
DanBokete <150482588+DanBokete@users.noreply.github.com>
2025-07-08 12:55:04 +0100

    fix: add  CORS origin URL for production deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:4173',
       'https://easy-study-8idasq6kr-dan-boketes-projects.vercel.app',
+      'https://easy-study-git-production-dan-boketes-projects.vercel.app',
     ],
     credentials: true,
   });
